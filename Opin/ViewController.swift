94a0c02b045f549bb68201df9e6a66823055ba24
Brian Vallelunga <vallelungabrian@gmail.com>
2015-09-12 21:15:13 -0700

    first commit

---
+++ Opin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Opin
+//
+//  Created by Brian Vallelunga on 9/12/15.
+//  Copyright (c) 2015 Brian Vallelunga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
